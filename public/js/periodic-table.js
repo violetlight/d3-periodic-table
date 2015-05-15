da7aafefdba28eb10ae9c333a530b52432311fc4
JF <jasonhfields@gmail.com>
2015-05-15 12:20:21 -0500

    better organized table

--- public/js/periodic-table.js
+++ public/js/periodic-table.js
@@ -1,5 +1,11 @@
 var svg = d3.select('.chart');
 
+// table formatting
+var square = $(document).width()/22;
+var border = 10;
+var leftOffset = 35;
+var topOffset = 35;
+
 svg.attr('width', function() {
   return $(document).width();
 }).attr('height', function() {
@@ -12,15 +18,14 @@ d3.json('/data/periodic-table.json', function(error, data){
   svg.selectAll('rect')
       .data(data)
     .enter().append('rect')
-      .attr('width', '90')
-      .attr('height', '90')
+      .attr('width', square)
+      .attr('height', square)
       .attr('x', function(d, i) {
-        return d.column * 100;
+        return ((d.column-1) * (square+border))+leftOffset;
       })
       .attr('y', function(d, i) {
-        return d.row * 100;
+        return ((d.row-1) * (square+border))+topOffset;
       })
       .attr('fill', '#FFF000')
-      .attr('fill-opacity', 1)
-      .text(function(d){ return d.name });
+      .attr('fill-opacity', 1);
 });
